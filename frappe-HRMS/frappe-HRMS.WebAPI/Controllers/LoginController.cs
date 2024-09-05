8a37d07f7101d82900b731e1af0cd57a0f93caf6
Dinesh kumar Narasingamoorthi <dineshkumar.narasingamoorthi@concertidc.com>
2024-09-05 15:44:22 +0530

    Token API done
    Token API done

--- frappe-HRMS/frappe-HRMS.WebAPI/Controllers/LoginController.cs
+++ frappe-HRMS/frappe-HRMS.WebAPI/Controllers/LoginController.cs
@@ -1,8 +1,11 @@
-﻿using frappe_HRMS.Domain;
-using frappe_HRMS.Services.Dto;
+﻿using frappe_HRMS.Services.Dto;
 using frappe_HRMS.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace frappe_HRMS.WebAPI.Controllers
 {
@@ -11,20 +14,124 @@ namespace frappe_HRMS.WebAPI.Controllers
     public class LoginController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IConfiguration configuration;
 
-        public LoginController(IUnitOfWork unitOfWork)
+        public LoginController(IUnitOfWork unitOfWork, IConfiguration configuration)
         {
             _unitOfWork = unitOfWork;
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        public  IActionResult Login([FromBody] LoginRequestDto request)
+        {
+            try
+            {
+                var user = _unitOfWork.Signup.GetAll().Result.Where(x => x.Email == request.Email && x.Password == request.Password).FirstOrDefault();
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+                var token = CreateToken(authClaims);
+                var refreshToken = GenerateRefreshToken();
+
+                var responseData = new
+                {
+                    accessToken = new JwtSecurityTokenHandler().WriteToken(token),
+                    RefreshToken = refreshToken,
+                };
+
+                return Ok(responseData);
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
         [HttpPost]
-        public async Task<ActionResult<string>> Login(LoginRequestDto request)
+        [Route("RefreshToken")]
+        public IActionResult RefreshToken(TokenModel tokenModel)
         {
-            var result = _unitOfWork.Signup.GetAll().Result.Where(x => x.Email == request.Email && x.Password == request.Password).FirstOrDefault();
-            if (result == null)
+            try
             {
-                return Ok("Invalid credentials provided.");
+                if (tokenModel is null)
+                {
+                    return BadRequest();
+                }
+
+                string accessToken = tokenModel.AccessToken;
+                string refreshToken = tokenModel.RefreshToken;
+
+                var principal = GetPrincipalFromExpiredToken(accessToken);
+                if (principal == null)
+                {
+                    return BadRequest();
+                }
+                var newAccessToken = CreateToken(principal.Claims.ToList());
+                var newRefreshToken = GenerateRefreshToken();
+                var responseData = new
+                {
+                    accessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
+                    RefreshToken = refreshToken,
+                };
+                return Ok(responseData);
             }
-            return Ok("Login Successfully!");          
+            catch (SecurityTokenExpiredException)
+            {
+                return Unauthorized();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
         }
+        #region supporting methods
+        private JwtSecurityToken CreateToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
+            _ = int.TryParse(configuration["JWT:TokenValidityInDays"], out int tokenValidityInDays);    
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JWT:ValidIssuer"],
+                audience: configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddDays(tokenValidityInDays),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return token;
+        }
+
+        private static string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        private ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                ValidateLifetime = false
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Invalid token");
+
+            return principal;
+        }
+        #endregion
     }
 }
